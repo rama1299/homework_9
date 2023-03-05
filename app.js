36b2e7f5d5aeffa53f2399d3cb8993ad5fdfba32
unknown <danramadhan0@gmail.com>
2023-03-06 02:24:38 +0700

    commit mulu

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 3100
 const router = require("./routes/index.js")
 const errorHandler = require("./middlewares/errorhandler.js")
+const swaggerUi = require ("swagger-ui-express")
+const moviesJson = require('./movies.json')
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(moviesJson))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
